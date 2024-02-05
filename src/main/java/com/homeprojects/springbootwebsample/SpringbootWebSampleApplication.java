434d70e965cab250de4bca4f819c240f1c659e9e
Jagan Ayyakutty <jagan26@gmail.com>
2024-02-05 10:34:13 +0000

    Intial commit - spring boot web app from Spring initializr

---
+++ src/main/java/com/homeprojects/springbootwebsample/SpringbootWebSampleApplication.java
@@ -0,0 +1,13 @@
+package com.homeprojects.springbootwebsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeprojects/springbootwebsample/SpringbootWebSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeprojects.springbootwebsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
